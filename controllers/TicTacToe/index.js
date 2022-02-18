8a2d6e6dd53c1b8273dfce4a3c6655003fa1f042
Tiago <tiagocrispim.m@gmail.com>
2022-02-18 01:18:11 +0000

    fixes

--- controllers/TicTacToe/index.js
+++ controllers/TicTacToe/index.js
@@ -1,113 +1,137 @@
-const fs = require('fs')
-const file = require('../../data.json');
+const fs = require("fs");
+const file = require("../../data.json");
 const ErrorResponse = require("../../utils/errorResponse");
 
 const validateGame = (player, x, y) => {
-
-    //check col
-    for(let i = 0; i < 3; i++){
-        if(file[x][i] !== player)
-            break;
-        if(i === 2){
-            return 'win'
-        }
+  //check col
+  for (let i = 0; i < 3; i++) {
+    if (file.board[x][i] !== player) break;
+    if (i === 2) {
+      return "win";
     }
+  }
 
-    //check row
-    for(let i = 0; i < 3; i++){
-        if(file[i][y] !== player)
-            break;
-        if(i === 2){
-            return 'win'
-        }
+  //check row
+  for (let i = 0; i < 3; i++) {
+    if (file.board[i][y] !== player) break;
+    if (i === 2) {
+      return "win";
     }
+  }
 
-    //check diag
-    if(x === y){
-        for(let i = 0; i < 3; i++){
-            if(file[i][i] !== player)
-                break;
-            if(i === 2){
-                return 'win'
-            }
-        }
+  //check diag
+  if (x === y) {
+    for (let i = 0; i < 3; i++) {
+      if (file.board[i][i] !== player) break;
+      if (i === 2) {
+        return "win";
+      }
     }
+  }
 
-    //check anti diag
-    if((x + y) === 2){
-        for(let i = 0; i < 2; i++){
-            if(file[i][2-i] !== player)
-                break;
-            if(i === 2){
-                return 'win'
-            }
-        }
+  //check anti diag
+  if (x + y === 2) {
+    for (let i = 0; i < 2; i++) {
+      if (file.board[i][2 - i] !== player) break;
+      if (i === 2) {
+        return "win";
+      }
     }
+  }
 
-    //check draw
+  //check draw
 
-
-    return false
-}
+  if (file.round == 9) {
+    return "draw";
+  }
+  return undefined;
+};
 
 exports.resetBoard = async (req, res, next) => {
-    try {
-        const data = [[null,null,null],[null,null,null],[null,null,null]]
-        fs.writeFileSync('data.json', JSON.stringify(data), function writeJSON(err) {
-            if (err) return next(new ErrorResponse(err, 500));
-        });
-        res.status(200).json(data);
-    }catch (e) {
-        next(new ErrorResponse())
-    }
-
-}
+  try {
+    const data = {
+      board: [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null],
+      ],
+      nextPlayer: "Player 1",
+      round: 1,
+    };
+    fs.writeFileSync(
+      "data.json",
+      JSON.stringify(data),
+      function writeJSON(err) {
+        if (err) return next(new ErrorResponse(err, 500));
+      }
+    );
+    res.status(200).json(data);
+  } catch (e) {
+    next(new ErrorResponse());
+  }
+};
 
 exports.getBoard = async (req, res, next) => {
-    try {
-        const data = fs.readFileSync('data.json')
-        const board = JSON.parse(data)
+  try {
+    const data = fs.readFileSync("data.json");
+    const board = JSON.parse(data);
 
-        res.status(200).json({board});
-    } catch (e) {
-        next(new ErrorResponse());
-    }
+    res.status(200).json(board);
+  } catch (e) {
+    next(new ErrorResponse());
+  }
 };
 
 exports.updateBoard = async (req, res, next) => {
-    try {
-        const { player, x, y } = req.body;
-
-        if(!player || !x || !y){
-            return next(new ErrorResponse('Player and square are required!', 404));
-        }
-
-        file[x][y] = player
-        fs.writeFileSync('data.json', JSON.stringify(file), function writeJSON(err) {
-            if (err) return next(new ErrorResponse(err, 500));
-        });
-
-        const result = validateGame(player, x, y)
-        let winner;
-        if(result){
-            if(result === 'win'){
-                winner = player;
-            }
-        }
-
-        const data = fs.readFileSync('data.json')
-        const board = JSON.parse(data)
-        console.log({
-            board,
-            winner,
-            result
-        })
-        res.status(200).json({
-            board,
-            winner,
-            result
-        });
-    } catch (e) {
-        next(new ErrorResponse());
+  try {
+    const { player, x, y } = req.body;
+    console.log(player, x, y);
+    let nextPlayer;
+    if (!player || x === undefined || x === undefined) {
+      return next(new ErrorResponse("Player and square are required!", 404));
     }
+    if (player === "Player 1") {
+      nextPlayer = "Player 2";
+    } else {
+      nextPlayer = "Player 1";
+    }
+
+    file.board[x][y] = player;
+    file.nextPlayer = nextPlayer;
+    file.round = file.round + 1;
+    fs.writeFileSync(
+      "data.json",
+      JSON.stringify(file),
+      function writeJSON(err) {
+        if (err) return next(new ErrorResponse(err, 500));
+      }
+    );
+
+    const result = validateGame(player, x, y);
+    console.log(result);
+    let winner;
+    if (result) {
+      if (result === "win") {
+        winner = player;
+      }
+    }
+
+    file.winner = winner;
+    file.result = result;
+
+    fs.writeFileSync(
+      "data.json",
+      JSON.stringify(file),
+      function writeJSON(err) {
+        if (err) return next(new ErrorResponse(err, 500));
+      }
+    );
+
+    const data = fs.readFileSync("data.json");
+    const game = JSON.parse(data);
+
+    res.status(200).json(game);
+  } catch (e) {
+    next(new ErrorResponse());
+  }
 };
